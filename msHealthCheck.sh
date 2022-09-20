35880e3dc80cc0afa7f9f2081a461129c05c7754
Nicolas Housset <nicolas.housset@gmail.com>
2022-09-20 17:08:29 +0200

    Update msHealthCheck.sh

--- msHealthCheck.sh
+++ msHealthCheck.sh
@@ -19,6 +19,52 @@ BLUE='\033[034m'
 NC='\033[0m' 
 
 
+for arg in "$@" ; do
+  case "$arg" in
+    --debug)
+      DEBUG=1;;
+    --os-packages-only)
+      OS_PACKAGES_ONLY=1;;
+    --install-only)
+      INSTALL_ONLY=1;;
+    --no-self-upgrade)
+      # Do not upgrade this script (also prevents client upgrades, because each
+      # copy of the script pins a hash of the python client)
+      NO_SELF_UPGRADE=1;;
+    --no-permissions-check)
+      NO_PERMISSIONS_CHECK=1;;
+    --no-bootstrap)
+      NO_BOOTSTRAP=1;;
+    --help)
+      HELP=1;;
+    --noninteractive|--non-interactive)
+      NONINTERACTIVE=1;;
+    --quiet)
+      QUIET=1;;
+    renew)
+      ASSUME_YES=1;;
+    --verbose)
+      VERBOSE=1;;
+    -[!-]*)
+      OPTIND=1
+      while getopts ":hnvq" short_arg $arg; do
+        case "$short_arg" in
+          h)
+            HELP=1;;
+          n)
+            NONINTERACTIVE=1;;
+          q)
+            QUIET=1;;
+          v)
+            VERBOSE=1;;
+        esac
+      done;;
+  esac
+done
+
+exit
+
+
 _GLOBAL_HTTPD_STATUS="KO"
 
 _GLOBAL_CONSUL_STATUS="KO"
