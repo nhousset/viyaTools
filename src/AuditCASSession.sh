a16465fc29c886bc979fb6e880855ac0055b8c0c
Nicolas Housset <nicolas.housset@gmail.com>
2023-06-06 13:14:56 +0200

    Update AuditCASSession.sh

--- src/AuditCASSession.sh
+++ src/AuditCASSession.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 
 CAS_DISK_CACHE_PATH=sastmp
-echo "UNIXTIMESTAMP;DATE;SAS_USER;PID;VSZ;RSS;UNIX_USER;NB_FILE_IN_CACHE" 
 while [ 1=1 ]
 do
   UnixDate=$(date +%s)
   FrenchDate=$(date '+%F %T');  
-      
+  nomServeur=$(hostname);
+  
   CASLogFile=$(ls -lrt /var/log/sas/viya/cas/default/ | grep cas_ | tail -1 | awk '{print $9}' )
  
   sommeCpu=0
@@ -23,10 +23,18 @@ do
    processRSS=$(echo $process | cut -d ";" -f 6)
    processStart=$(echo $process | cut -d ";" -f 7)
    
-   sasUser=$(grep "Launched session worker. Process ID is $processPID" /var/log/sas/viya/cas/default/$CASLogFile | tail -1 | awk '{print $5}'  )
+   sasUser=$(grep "Launched session" /var/log/sas/viya/cas/default/$CASLogFile |  grep "Process ID is $processPID" | tail -1 | awk '{print $5}'  )
+   
+   
+   memTotal=$(free | grep -v total | grep -v Swap | awk '{print $2}' )
+   memUsed=$(free | grep -v total | grep -v Swap | awk '{print $3}' )
+   memFree=$(free | grep -v total | grep -v Swap | awk '{print $4}' )
+   memShared=$(free | grep -v total | grep -v Swap | awk '{print $5}' )
+   memCache=$(free | grep -v total | grep -v Swap | awk '{print $6}' )
+   memAvailable=$(free | grep -v total | grep -v Swap | awk '{print $7}' )
    
    NB_FILE_MAP=$(lsof -p $processPID | grep $CAS_DISK_CACHE_PATH |wc -l)
-   echo $UnixDate";"$FrenchDate";"$sasUser";"$processPID";"$processVSZ";"$processRSS";"$processUser";"$NB_FILE_MAP
+   echo $nomServeur";"$UnixDate";"$FrenchDate";"$sasUser";"$processPID";"$processVSZ";"$processRSS";"$processUser";"$NB_FILE_MAP";"$NB_FILE_MAP";"$memTotal";"$memUsed";"$memFree";"$memShared";"$memCache";"$memAvailable
    
   
   done
