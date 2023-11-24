c928dae7d49d54e05192e03809628f6aaa1c47b5
Nicolas Housset <nicolas.housset@gmail.com>
2023-11-24 09:02:05 +0100

    Update AuditCASSession.sh

--- src/AuditCASSession.sh
+++ src/AuditCASSession.sh
@@ -1,19 +1,20 @@
 #!/bin/bash 
 
-CAS_DISK_CACHE_PATH=cas_cache_disk
-echo "nomServeur,UnixDate,FrenchDate,sasUser,processPID,processVSZ,processRSS,processUser,NB_FILE_MAP,SIZE_IN_CACHE,memTotal,memUsed,memFree,memShared,memCache,memAvailable,sasDate,processVSZGO,processRSSGO,SIZE_IN_CACHE_GO"
+export CAS_DISK_CACHE_PATH="/sastmp"
+export LOG_FILE="/tmp/auditCASSession.csv";
+echo "nomServeur,UnixDate,FrenchDate,sasUser,processPID,processVSZ,processRSS,processUser,NB_FILE_MAP,SIZE_IN_CACHE,memTotal,memUsed,memFree,memShared,memCache,memAvailable,sasDate,processVSZGO,processRSSGO,SIZE_IN_CACHE_GO" | tee -a $LOG_FILE
 while [ 1=1 ]
 do
   UnixDate=$(date +%s)
   FrenchDate=$(date '+%F %T');  
   nomServeur=$(hostname);
   
-  CASLogFile=$(ls -lrt /var/log/sas/viya/cas/default/ | grep cas_ | tail -1 | awk '{print $9}' )
+  CASLogFile=$(ls -lrt /var/log/sas/viya/cas/server02/ | grep cas_ | tail -1 | awk '{print $9}' )
  
   sommeCpu=0
   nbSasProcess=0
   i=0
-  for process in $(ps -aux | grep -v root | grep "cas session" | awk '{print $1";"$2";"$3";"$4";"$5";"$6";"$8";"$9}'  |  grep -v grep )
+  for process in $(ps -aux | grep -v root | grep "cas session" |  grep -v grep | awk '{print $1";"$2";"$3";"$4";"$5";"$6";"$8";"$9}'   )
   do
   
    processUser=$(echo $process | cut -d ";" -f 1)
@@ -28,8 +29,8 @@ do
    processVSZGO=$(echo $processVSZ | awk '{printf("%13.2f",$1/1024/1024)}')  
    processRSSGO=$(echo $processRSS  | awk '{printf("%13.2f",$1/1024/1024)}')  
    
-   sasUser=$(grep "Launched session" /var/log/sas/viya/cas/default/$CASLogFile |  grep "Process ID is $processPID" | tail -1 | awk '{print $5}'  )
    
+   sasUser=$(grep "Launched session" /var/log/sas/viya/cas/server02/$CASLogFile |  grep "Process ID is $processPID" | tail -1 | awk '{print $5}'  )
    
    memTotal=$(free | grep -v total | grep -v Swap | awk '{print $2}'| awk '{printf("%13.2f",$1/1024/1024)}' )
    memUsed=$(free | grep -v total | grep -v Swap | awk '{print $3}'| awk '{printf("%13.2f",$1/1024/1024)}' )
@@ -46,13 +47,11 @@ do
    typeset -i sasDate=0
      
    sasDate=`expr $UnixDate-315360000`
-   echo $nomServeur","$UnixDate","$FrenchDate","$sasUser","$processPID","$processVSZ","$processRSS","$processUser","$NB_FILE_MAP","$SIZE_IN_CACHE","$memTotal","$memUsed","$memFree","$memShared","$memCache","$memAvailable","$sasDate","$processVSZGO","$processRSSGO","$SIZE_IN_CACHE_GO
+   echo $nomServeur","$UnixDate","$FrenchDate","$sasUser","$processPID","$processVSZ","$processRSS","$processUser","$NB_FILE_MAP","$SIZE_IN_CACHE","$memTotal","$memUsed","$memFree","$memShared","$memCache","$memAvailable","$sasDate","$processVSZGO","$processRSSGO","$SIZE_IN_CACHE_GO | tee -a $LOG_FILE
    
-  
+  chmod 777 $LOG_FILE
+	
   done
   sleep 10
   
  done
- 
- 
-
